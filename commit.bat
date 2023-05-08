fa1001d5691b8ea1268963cfee4044e64044a47e
Akash Mittal <mittal.akash.adam@gmail.com>
2023-05-08 13:14:38 +0200

    Added new HTML files

---
+++ commit.bat
@@ -0,0 +1,16 @@
+@echo off
+setlocal
+
+set GITHUB_USERNAME=Akash-Mittal
+set GITHUB_TOKEN=%GITHUB_TOKEN%
+set GITHUB_REPO=mittal.blog
+
+git pull https://%GITHUB_USERNAME%:%GITHUB_TOKEN%@github.com/%GITHUB_USERNAME%/%GITHUB_REPO%.git
+git fetch https://%GITHUB_USERNAME%:%GITHUB_TOKEN%@github.com/%GITHUB_USERNAME%/%GITHUB_REPO%.git
+
+
+git add *
+
+git commit -m "Added new HTML files"
+git push https://%GITHUB_USERNAME%:%GITHUB_TOKEN%@github.com/%GITHUB_USERNAME%/%GITHUB_REPO%.git
+endlocal
