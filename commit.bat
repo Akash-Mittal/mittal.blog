fda5e886c3eae67b8d93f27ef8fbee7c8ab35ac4
Akash Mittal <mittal.akash.adam@gmail.com>
2023-05-09 14:14:17 +0200

    Added new HTML files

--- commit.bat
+++ commit.bat
@@ -1,3 +1,5 @@
+git pull
+git fetch
 @echo off
 setlocal
 
@@ -9,7 +11,7 @@ git pull https://%GITHUB_USERNAME%:%GITHUB_TOKEN%@github.com/%GITHUB_USERNAME%/%
 git fetch https://%GITHUB_USERNAME%:%GITHUB_TOKEN%@github.com/%GITHUB_USERNAME%/%GITHUB_REPO%.git
 
 
-git add *
+git add *.*
 
 git commit -m "Added new HTML files"
 git push https://%GITHUB_USERNAME%:%GITHUB_TOKEN%@github.com/%GITHUB_USERNAME%/%GITHUB_REPO%.git
